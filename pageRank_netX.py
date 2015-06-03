5db7eb2745fe4a233286152a225b485059469053
qzshadow <qz1431880590@gmail.com>
2015-06-03 10:58:30 +0800

    rewrite using networkx rather than pygraph, it is cool! final version 2

--- pageRank_netX.py
+++ pageRank_netX.py
@@ -1,31 +1,28 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 22 21:26:36 2015
+__author__ = 'Qin'
 
-@author: Qin
-"""
+import networkx as nx
 
-from pygraph.classes.digraph import digraph
-
+# Graph creation
+G = nx.DiGraph()
 
 # Define pagerank function
 def pagerank(graph, damping_factor=0.85, max_iterations=100,
              min_delta=0.00001):
     """
-    Compute and return the PageRank in an directed graph.    
-    
+    Compute and return the PageRank in an directed graph.
+
     @type  graph: digraph
     @param graph: Digraph.
-    
+
     @type  damping_factor: number
     @param damping_factor: PageRank dumping factor.
-    
-    @type  max_iterations: number 
+
+    @type  max_iterations: number
     @param max_iterations: Maximum number of iterations.
-    
+
     @type  min_delta: number
     @param min_delta: Smallest variation required for a new iteration.
-    
+
     @rtype:  Dict
     @return: Dict containing all the nodes PageRank.
     """
@@ -46,7 +43,7 @@ def pagerank(graph, damping_factor=0.85, max_iterations=100,
         # computes each node PageRank based on inbound links
         for node in nodes:
             rank = min_value
-            for referring_page in graph.incidents(node):
+            for referring_page in graph.predecessors(node):
                 rank += damping_factor * pagerank[referring_page] / \
                         len(graph.neighbors(referring_page))
 
@@ -63,29 +60,23 @@ def pagerank(graph, damping_factor=0.85, max_iterations=100,
 
     return pagerank
 
-# Graph creation
-gr = digraph()
 
 # Add nodes and edges
 with open('nodes', mode='r', encoding='utf8') as nodesfile:
     for node in nodesfile:
-        gr.add_node(node.strip())
+        G.add_node(node.strip())
 
 with open('edges', mode='r', encoding='utf8') as edgesfile:
     for edge in edgesfile:
-        from_edge,to_edge = edge.split('\t')
-        gr.add_edge((from_edge.strip(),to_edge.strip()))
+        from_edge, to_edge = edge.split('\t')
+        G.add_edge(from_edge.strip(), to_edge.strip())
 
-##Draw as PNG
-# from pygraph.readwrite.dot import write
-# import graphviz as gv
-# dot = write(gr)
-# gvv = gv.readstring(dot)
-# gv.layout(gvv,'dot')
-# gv.render(gvv,'png','Model.png')
-
-pagerank = pagerank(gr)
-# print(pagerank)
+pagerank = pagerank(G)
 with open('pagerank', mode='w', encoding='utf8') as pagerankfile:
     for (key,value) in sorted(pagerank.items(),key=lambda x:x[1], reverse=True):
         pagerankfile.write('{0}\t{1}\n'.format(value,key))
+
+# draw the graph
+# import matplotlib.pyplot as plt
+# nx.draw(G)
+# plt.savefig("nodes.png")
